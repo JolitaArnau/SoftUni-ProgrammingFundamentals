584caf430f9bb3575a58a14de42fd30933663974
unknown <jolita.arnaudova@gmail.com>
2017-09-29 12:43:46 +0200

    programm, that reads some info about products and their prices in a cafe, stores them in a dict, then reads customers and their orders, checks if the order is present in the store, and if so, adds them to customers shopping list, finally prints bill and total bill

---
+++ ObjectsAndClasses/Pr07AndreyAndBilliard/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace Pr07AndreyAndBilliard
+{
+    using System.Collections.Generic;
+
+    class Customer
+    {
+        public Dictionary<string, Dictionary<string,int>> ShoppingList { get; set; }
+
+        public decimal Bill { get; set; }
+    }
+}
---
+++ ObjectsAndClasses/Pr07AndreyAndBilliard/Pr07AndreyAndBilliard.cs
@@ -0,0 +1,91 @@
+﻿namespace Pr07AndreyAndBilliard
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    class Pr07AndreyAndBilliard
+    {
+        public static void Main()
+        {
+            int countOfEntities = int.Parse(Console.ReadLine());
+
+            var shop = new Dictionary<string, decimal>();
+
+            for (int i = 0; i < countOfEntities; i++)
+            {
+                var currentEntity = Console.ReadLine().Split('-').ToList();
+
+                var product = currentEntity[0];
+                var productPrice = decimal.Parse(currentEntity[1]);
+
+                if (!shop.ContainsKey(product))
+                {
+                    shop[product] = productPrice;
+                }
+                else
+                {
+                    shop[product] = productPrice;
+                }
+            }
+
+            var orders = Console.ReadLine();
+
+            Customer customer = new Customer();
+
+            var shoppingList = customer.ShoppingList = new Dictionary<string, Dictionary<string, int>>();
+
+            while (!orders.Equals("end of clients"))
+            {
+                var currentOrder = orders.Split(new char[] { '-', ',' }).ToList();
+                var currentCustomerName = currentOrder[0];
+                var currentOrderProduct = currentOrder[1];
+                var currentProductQuantity = int.Parse(currentOrder[2]);
+
+
+                if (shop.ContainsKey(currentOrderProduct))
+                {
+                    if (!shoppingList.ContainsKey(currentCustomerName))
+                    {
+                        shoppingList[currentCustomerName] = new Dictionary<string, int>();
+
+                        if (!shoppingList[currentCustomerName].ContainsKey(currentOrderProduct))
+                        {
+                            shoppingList[currentCustomerName][currentOrderProduct] = currentProductQuantity;
+                            
+                        }
+
+                    }
+                }
+
+                orders = Console.ReadLine();
+            }
+
+            var currentBill = customer.Bill;
+            var totalBill = 0.0M;
+
+            foreach (var student in shoppingList.OrderBy(s => s.Key))
+            {
+                Console.WriteLine(student.Key);
+
+                foreach (var order in student.Value)
+                {
+                    foreach (var item in shop)
+                    {
+                        if (order.Key.Equals(item.Key))
+                        {
+                            currentBill = item.Value * order.Value;
+                        }
+                    }
+
+                    Console.WriteLine($"-- {order.Key} - {order.Value}");
+                    Console.WriteLine($"Bill: {currentBill:f2}");
+                }
+
+                totalBill += currentBill;
+            }
+
+            Console.WriteLine($"Total bill: {totalBill:f2}");
+        }
+    }
+}
